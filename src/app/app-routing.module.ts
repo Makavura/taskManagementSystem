a40e802620195da607dc2d58f2975c9c6d636e80
Makavura <makavuramughanga@gmail.com>
2019-06-29 16:16:25 +0300

    create tasks UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {CreateUserComponent} from './create-user/create-user.component';
+import {TasksComponent} from './tasks/tasks.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  {path: 'user', component: CreateUserComponent},
+  {path: 'tasks', component: TasksComponent},
+  {path: '**', component: PageNotFoundComponent},
+  {path: '/', redirectTo: '/user', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
